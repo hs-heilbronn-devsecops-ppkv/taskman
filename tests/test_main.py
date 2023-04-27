7b2b2fad75d6f9ecb1dbd1e93cf335faca4add59
Pratik Patil <patilpratik.android@gmail.com>
2023-04-27 21:58:56 +0000

    Commit to test imports

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,6 @@
 from taskman.main import create_task, get_task, get_tasks,TaskRequest, Task, delete_tasks
+from fastapi.testclient import TestClient
+from taskman.main import app
 
 
 def test_save_and_get_item():
