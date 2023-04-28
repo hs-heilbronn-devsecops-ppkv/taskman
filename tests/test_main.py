3e37a8ae43f15d01274826cc0dd6c6cb68db15a4
pradnya jadhav <jadhav.pradnya.0110@gmail.com>
2023-04-28 14:23:16 +0000

    test main file updated

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,16 @@
 from taskman.main import create_task, get_task, get_tasks,TaskRequest, Task, delete_tasks
-
+from fastapi.testclient import TestClient
+from taskman.main import app
+import time
 
 def test_save_and_get_item():
     delete_tasks()
+    print("Running test for save task")
     create_task(TaskRequest(
         name='Test Task',
         description='Demo',
     ))
     assert get_task('1') == Task(name='Test Task', description='Demo', item_id=1)
-
 
 #test api for getting tasks
 def test_get_tasks():
@@ -38,4 +40,6 @@ def factorial(n):
         return n * factorial(n-1)
 
 def test_factorial_of_3():
+    print("Running fact of 3")
     assert factorial(3) == 6
+
